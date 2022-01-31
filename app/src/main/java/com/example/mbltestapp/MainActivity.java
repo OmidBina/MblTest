cc63e0c7cb1dc7d049b48902ffae24676826e83b
omid-mac <omid36163413@gmail.com>
2022-01-31 20:47:48 +0330

    GameView added and gameActivitys background movements added

--- app/src/main/java/com/example/mbltestapp/MainActivity.java
+++ app/src/main/java/com/example/mbltestapp/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.mbltestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView play_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        elements();
+    }
+
+    private void elements() {
+
+        play_btn = findViewById(R.id.main_play_btn);
+
+        manage_clicks();
+    }
+
+    private void manage_clicks() {
+
+        play_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+
+            }
+        });
+
     }
 }
+
